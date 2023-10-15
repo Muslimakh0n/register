8417821ca3d27419487d61e2ac025170a67f6cbb
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-10-15 22:42:40 +0500

    register , agaiiiin

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 // import "../index.css"
 // idk what and why i did it , my tailwind not working tho
 
-const  Registration = () => {
+const Registration = () => {
   return (
-    <div className=' h-screen w-screen '>
-      <div className=' mx-auto h-[600px] w-[500px] p-4 bg-neutral-500'>
-        <h1 className=' text-[30px] mb-4'>Register</h1>
-        <form>
-          <label htmlFor='username' className='text-2xl mb-2 block'>UserName</label>
-          <input type="text" className='w-full mb-3'/>
-          <label htmlFor="password" className='text-2xl mb-2 block'>Password</label>
-          <input type="password" autoComplete='new-password' className='w-full mb-3' />
-          <button>Send</button>
+    <div className='flex items-center justify-center h-screen'>
+      <div className='bg-gray-800 p-8 rounded-lg shadow-lg w-96'>
+        <h1 className='text-3xl mb-4 text-white'>Register</h1>
+        <form className='space-y-4'>
+          <div>
+            <label htmlFor='username' className='text-lg mb-2 block text-white'>UserName</label>
+            <input type="text" className='w-full p-2 border rounded focus:outline-none focus:border-blue-500' />
+          </div>
+          <div>
+            <label htmlFor="password" className='text-lg mb-2 block text-white'>Password</label>
+            <input type="password" autoComplete='new-password' className='w-full p-2 border rounded focus:outline-none focus:border-blue-500' />
+          </div>
+          <button className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>Register</button>
         </form>
-        <p>Already have an account?</p>
-        <a href="#" className='underline'>Log In</a>
+        <p className='mt-4 text-white'>Already have an account?</p>
+        <a href="#" className='underline text-blue-500'>Log In</a>
       </div>
     </div>
-  )
+  );
 }
 
-export default Registration
+export default Registration;
